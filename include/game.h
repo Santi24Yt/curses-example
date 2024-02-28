f78d9a67e995a42397736a80f0cb2363e14110b3
Santi24Yt <santi24yt@gmail.com>
2024-02-28 10:16:20 -0600

    headers

--- include/game.h
+++ include/game.h
@@ -9,8 +9,8 @@
 /* Define la posición de algo */
 typedef struct
 {
-  int x;
   int y;
+  int x;
 } position;
 
 /* Una entidad, tiene posición y un caracter que la representa */
@@ -20,14 +20,44 @@ typedef struct
   char ch;
 } entity;
 
+/* Un tile, puede ser suelo o no */
+typedef struct
+{
+  char ch;
+  /* stdbool posiblemente viene incluido en curses */
+  bool walkable;
+} tile;
+
+
+/* Definir la firma de las funciones de engine.c */
+void setup();
+void loop();
+void close();
 
 /* Definir la firma de las funciones de player.c */
 entity* create_player(position startpos);
 void handle_input(char in);
+void move_player(position pos);
 
+/* Definir la firma de las funciones de draw.c */
+void draw_map();
+void draw_ent(entity* ent);
+void draw();
+
+/* Definir la firma de las funciones de map.c */
+tile** create_maptiles();
+position map1();
+void freemap();
+
+/* Variables externas / globales */
 /* Incluir a la variable que va a ser el jugador principal
  * extern hace que se pueda usar en varios archivos */
 extern entity* player;
+/* Constantes del tamaño del mapa */
+extern const int MAP1_HEIGHT;
+extern const int MAP1_WIDTH;
+/* Un puntero de punteros que incluya los tiles del mapa, aka. un arreglo de arreglos de tile */
+extern tile** map;
 
 /* Acaba el if que checa si ya fue compilado */
 #endif
